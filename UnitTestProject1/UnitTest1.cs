d4bda1d578d515f1238a4d92bb25d1d4f8c4222b
Helmut Theis <helmut.theis@hypermediasystems.de>
2016-12-14 21:46:30 +0100

    Test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "Hallo world!";
+
+            string output = toUpper.run(input);
+            Assert.AreEqual(output, input,"not correct");
+        }
+    }
+}
