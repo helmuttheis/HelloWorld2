01ee9f1218eee47475983fb3a1d732d2c97d5dd1
Helmut Theis <helmut.theis@hypermediasystems.de>
2016-12-14 21:36:08 +0100

    neu Libraray

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ClassLibrary1;
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world")
+            Console.WriteLine(toUpper.run( "Hello world"));
         }
     }
 }
